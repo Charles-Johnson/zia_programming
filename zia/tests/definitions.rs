79d3cd2054727d5fd2dc6a911098b06848e87b52
Charles Johnson <charlesthomasjohnson0@gmail.com>
2019-01-14 16:45:30 +0900

    Begin property based testing with `proptest`
    Instead of only testing particular cases for the integration tests,
    the `proptest` crate can be used to test for a range of cases and
    find the simplest case of failure.
    
    Apply to first few tests in `tests/definitions.rs`.

--- zia/tests/definitions.rs
+++ zia/tests/definitions.rs
@@ -14,39 +14,123 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+#[macro_use] 
+extern crate proptest;
 extern crate zia;
 
 use zia::{Context, ContextMaker, Execute, ZiaError};
-#[test]
-fn fresh_symbol() {
-    let mut cont = Context::new();
-    assert_eq!(cont.execute("(label_of (a :=)) ->"), "a");
+
+macro_rules! assume_symbol {
+	($a:ident) => (
+		prop_assume!($a.len() > 0);
+		prop_assume!(!$a.contains(' '));
+		prop_assume!(!$a.contains('('));
+		prop_assume!(!$a.contains(')'));
+	)
 }
-#[test]
-fn fresh_pair() {
-    let mut cont = Context::new();
-    assert_eq!(cont.execute("let (a (:= (b c)))"), "");
-    assert_eq!(cont.execute("(label_of (a :=)) ->"), "b c");
-}
-#[test]
-fn fresh_nested_pairs() {
-    let mut cont = Context::new();
-    assert_eq!(cont.execute("let (a (:= (b (c d))))"), "");
-    assert_eq!(cont.execute("(label_of (a :=)) ->"), "b (c d)");
-}
-#[test]
-fn defining_used_symbol_as_fresh_pair() {
-    let mut cont = Context::new();
-    assert_eq!(cont.execute("let (a (:= (b c)))"), "");
-    assert_eq!(cont.execute("let (b (:= (d e)))"), "");
-    assert_eq!(cont.execute("(label_of (b :=)) ->"), "d e");
-}
-#[test]
-fn defining_fresh_symbol_as_used_pair() {
-    let mut cont = Context::new();
-    assert_eq!(cont.execute("let (a (:= (b c)))"), "");
-    assert_eq!(cont.execute("let (d (:= (b c)))"), "");
-    assert_eq!(cont.execute("(label_of (d :=)) ->"), "b c");
+
+proptest! {
+	// The label of a new symbol should reduce to the string of the symbol. 
+	#[test]
+	fn fresh_symbol(a in "\\PC*") {
+		assume_symbol!(a);
+		let mut cont = Context::new();
+		let command = format!("(label_of ({} :=)) ->", a);
+		prop_assert_eq!(cont.execute(&command), a);
+	}
+	// The label of the expansion of a previously unused concept which is composed of a pair of previously unused concepts should reduce to the string representation of the pair. 
+	#[test]
+	fn fresh_pair(a in "\\PC*", b in "\\PC*", c in "\\PC*") {
+		prop_assume!(a != ":=");
+		assume_symbol!(a);
+		assume_symbol!(b);
+		assume_symbol!(c);
+		prop_assume!(a != b);
+		prop_assume!(a != c);
+		let mut cont = Context::new();
+		let let_command = format!("let ({} (:= ({} {})))", a, b, c);
+		assert_eq!(cont.execute(&let_command), "");
+		let print_command = format!("(label_of ({} :=)) ->", a);
+		let result = format!("{} {}", b, c);
+		assert_eq!(cont.execute(&print_command), result);
+	}
+	// The label of the expansion of a previosuly unused concept which is composed of a nested pair of previously unused concepts should reduce to the string representation of the nested pair. 
+	#[test]
+	fn fresh_nested_pairs(a in "\\PC*", b in "\\PC*", c in "\\PC*", d in "\\PC*") {
+		prop_assume!(a != ":=");
+		assume_symbol!(a);
+		assume_symbol!(b);
+		assume_symbol!(c);
+		assume_symbol!(d);
+		prop_assume!(a != b);
+		prop_assume!(a != c);
+		prop_assume!(a != d);
+		let mut cont = Context::new();
+		let let_command = format!("let ({} (:= ({} ({} {}))))", a, b, c, d);
+		assert_eq!(cont.execute(&let_command), "");
+		let print_command = format!("(label_of ({} :=)) ->", a);
+		let result = format!("{} ({} {})", b, c, d);
+		assert_eq!(cont.execute(&print_command), result);
+	}
+	// The label of the expansion of a previously used concept which is composed of a pair of previously unused concepts should reduce to the string representation of the pair. 
+	#[test]
+	fn defining_used_symbol_as_fresh_pair(
+		a in "\\PC*", 
+		b in "\\PC*", 
+		c in "\\PC*", 
+		d in "\\PC*",
+		e in "\\PC*",
+	) {
+		prop_assume!(a != ":=");
+		assume_symbol!(a);
+		assume_symbol!(b);
+		assume_symbol!(c);
+		assume_symbol!(d);
+		assume_symbol!(e);
+		prop_assume!(a != b);
+		prop_assume!(a != c);
+		prop_assume!(a != d);
+		prop_assume!(a != e);
+		prop_assume!(b != d);
+		prop_assume!(b != e);
+		prop_assume!(c != d);
+		prop_assume!(c != e);
+		let mut cont = Context::new();
+		let first_let_command = format!("let ({} (:= ({} {})))", a, b, c);
+		assert_eq!(cont.execute(&first_let_command), "");
+		let second_let_command = format!("let ({} (:= ({} {})))", b, d, e);
+		assert_eq!(cont.execute(&second_let_command), "");
+		let print_command = format!("(label_of ({} :=)) ->", b);
+		let result = format!("{} {}", d, e);
+		assert_eq!(cont.execute(&print_command), result);
+	}
+	// The label of the expansion of a previously unused concept which is composed of a pair of previously used concepts should reduce to the string representation of the pair.
+	#[test]
+	fn defining_fresh_symbol_as_used_pair(
+		a in "\\PC*", 
+		b in "\\PC*", 
+		c in "\\PC*", 
+		d in "\\PC*",
+	) {
+		assume_symbol!(a);
+		assume_symbol!(b);
+		assume_symbol!(c);
+		assume_symbol!(d);
+		prop_assume!(a != ":=");
+		prop_assume!(a != b);
+		prop_assume!(a != c);
+		prop_assume!(d != b);
+		prop_assume!(d != c);
+		prop_assume!(a != d);
+		let mut cont = Context::new();
+		let first_let_command = format!("let ({} (:= ({} {})))", a, b, c);
+		assert_eq!(cont.execute(&first_let_command), "");
+		let second_let_command = format!("let ({} (:= ({} {})))", d, b, c);
+		assert_eq!(cont.execute(&second_let_command), "");
+		let print_command = format!("(label_of ({} :=)) ->", d);
+		let result = format!("{} {}", b, c);
+		assert_eq!(cont.execute(&print_command), result);
+	}
 }
 #[test]
 fn old_pair() {
