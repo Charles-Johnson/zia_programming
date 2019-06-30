f2254ceaf03d0d09b2fb9fb2734c4ff8b3a11343
Charles Johnson <charlesthomasjohnson0@gmail.com>
2019-06-30 20:18:16 +0900

    replace match statement with and_then

--- zia/src/translating.rs
+++ zia/src/translating.rs
@@ -119,10 +119,7 @@ where
     T: FindWhatReducesToIt + GetDefinition,
 {
     fn concept_from_label(&self, s: &str) -> Option<usize> {
-        match self.get_string_concept(s) {
-            None => None,
-            Some(c) => self.get_labellee(&[], c),
-        }
+        self.get_string_concept(s).and_then(|c| self.get_labellee(&[], c))
     }
     fn ast_from_symbol<U: From<(String, Option<usize>)>>(&self, s: &str) -> U {
         let concept_if_exists = self.concept_from_label(s);
