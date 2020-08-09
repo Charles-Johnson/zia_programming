5520dc58c8bf91000a9b769d413b8d64074a828f
Charles Johnson <charlesthomasjohnson0@gmail.com>
2020-08-09 16:05:59 +0900

    :white_check_mark: convert variables proptests to normal tests

--- zia/tests/variables.rs
+++ zia/tests/variables.rs
@@ -13,29 +13,23 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-#[macro_use]
-extern crate proptest;
 extern crate zia;
 
 use zia::NEW_CONTEXT;
 
-proptest! {
-    #[test]
-    fn single_variable_reduction(a in "a|b|c|d", b in "a|b|c|d", c in "a|b|c|d", d in "a|b|c|d") {
-        let mut context = NEW_CONTEXT.clone();
-        assert_eq!(context.execute(&format!("let _{}_ {} -> {}", a, b, c)), "");
-        assert_eq!(context.execute(&format!("{} {}", d, b)), c);
-    }
-    #[test]
-    fn repeated_variable_reduction(a in "a|b|c", b in "a|b|c", c in "a|b|c") {
-        prop_assume!(b != c);
-        let mut context = NEW_CONTEXT.clone();
-        // Define how a + a can be written as 2 a
-        assert_eq!(context.execute(&format!("let _{0}_ + _{0}_ -> 2 _{0}_", a)), "");
-        // Check whether a + b -> 2 a if a = b
-        assert_eq!(context.execute(&format!("{0} + {0}", b)), format!("2 {}", b));
-        // Check whether a + b doesn't reduce if a != b
-        assert_eq!(context.execute(&format!("{} + {}", b, c)), format!("{} + {}", b, c));
-    }
+#[test]
+fn single_variable_reduction() {
+    let mut context = NEW_CONTEXT.clone();
+    assert_eq!(context.execute("let _a_ b -> c"), "");
+    assert_eq!(context.execute("d b"), "c");
+}
+#[test]
+fn repeated_variable_reduction() {
+    let mut context = NEW_CONTEXT.clone();
+    // Define how a + a can be written as 2 a
+    assert_eq!(context.execute("let _a_ + _a_ -> 2 _a_"), "");
+    // Check whether a + b -> 2 a if a = b
+    assert_eq!(context.execute("b + b"), "2 b");
+    // Check whether a + b doesn't reduce if a != b
+    assert_eq!(context.execute("b + c"), "b + c");
 }
