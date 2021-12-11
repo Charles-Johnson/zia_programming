7ad2cc1b8f1c79a7c1dc366a466976eea5b0f263
Charles Johnson <charlesthomasjohnson0@gmail.com>
2021-12-11 21:20:08 +0900

    Make test not require as much API surface area

--- zia/tests/lexer.rs
+++ zia/tests/lexer.rs
@@ -40,11 +40,7 @@ proptest! {
 #[test]
 fn lexer_indentifies_concrete_concept() {
     let cont = NEW_CONTEXT.clone();
-        assert_eq!(
-            cont.lex("let"),
-            vec![Lexeme {
-                text: "let".into(),
-                category: LexemeCategory::ConcreteConcept
-            }]
-        );
+    let lexemes = cont.lex("let");
+    assert_eq!(lexemes.len(), 1);
+    assert_eq!(lexemes.first().unwrap().category, LexemeCategory::ConcreteConcept);
 }
