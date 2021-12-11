301f28cc280cee697bdd20289409be1367573a60
Charles Johnson <charlesthomasjohnson0@gmail.com>
2021-12-11 21:08:54 +0900

    Gerenalise test of whitespace lexing with proptest

--- zia/tests/lexer.rs
+++ zia/tests/lexer.rs
@@ -20,17 +20,19 @@ fn lexer_indentifies_new_concept() {
     assert_eq!(lexemes.first().unwrap().category, LexemeCategory::NewConcept);
 }
 
-#[test]
-fn lexer_indentifies_whitespace() {
-    let cont = NEW_CONTEXT.clone();
-    let lexemes = cont.lex("new_concept another_new_concept");
-    assert_eq!(lexemes.len(), 3);
-    assert_eq!(lexemes[0].text, "new_concept");
-    assert_eq!(lexemes[0].category, LexemeCategory::NewConcept);
+proptest! {
+    #[test]
+    fn lexer_indentifies_whitespace(w in r"\s") {
+        let cont = NEW_CONTEXT.clone();
+        let lexemes = cont.lex(&format!("new_concept{}another_new_concept", w));
+        assert_eq!(lexemes.len(), 3);
+        assert_eq!(lexemes[0].text, "new_concept");
+        assert_eq!(lexemes[0].category, LexemeCategory::NewConcept);
 
-    assert_eq!(lexemes[1].text, " ");
-    assert_eq!(lexemes[1].category, LexemeCategory::Whitespace);
+        assert_eq!(lexemes[1].text, w);
+        assert_eq!(lexemes[1].category, LexemeCategory::Whitespace);
 
-    assert_eq!(lexemes[2].text, "another_new_concept");
-    assert_eq!(lexemes[2].category, LexemeCategory::NewConcept);
+        assert_eq!(lexemes[2].text, "another_new_concept");
+        assert_eq!(lexemes[2].category, LexemeCategory::NewConcept);
+    }
 }
