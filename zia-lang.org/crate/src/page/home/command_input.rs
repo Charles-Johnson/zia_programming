9f04b1671eac6d0594fe584f03f85c301c8c5f0b
Charles Johnson <charlesthomasjohnson0@gmail.com>
2022-01-04 23:38:17 +0900

    Implement syntax colouring for website

--- zia-lang.org/crate/src/page/home/command_input.rs
+++ zia-lang.org/crate/src/page/home/command_input.rs
@@ -2,7 +2,8 @@ use super::{Model as HomeModel, Msg as HomeMsg, EDGE_STYLE, TEXT_PADDING};
 use crate::{
     generated::css_classes::C, page::home::OUTER_PADDING, Msg as GlobalMsg,
 };
-use seed::{attrs, div, prelude::*, style, textarea, C};
+use seed::{attrs, div, prelude::*, span, style, textarea, C};
+use zia::{ConceptKind, LexemeCategory};
 
 pub fn view(model: &HomeModel) -> impl IntoNodes<GlobalMsg> {
     let height = model.command_input.get().map_or_else(
@@ -13,26 +14,29 @@ pub fn view(model: &HomeModel) -> impl IntoNodes<GlobalMsg> {
         |e| px(e.scroll_height()),
     );
     // prevents bottom part of history being obscured by command input
-    let div = div![style![St::Height => height]];
+    let bottom_buffer = div![style![St::Height => height],];
+    let textarea_class = C![
+        C.border_primary,
+        C.border_2,
+        EDGE_STYLE,
+        TEXT_PADDING,
+        C.outline_none,
+        C.overflow_hidden
+    ];
+    let textarea_style = style! {St::Resize => "none", St::Height => height,
+        St::Position => "fixed",
+        // required to fix the command input to the bottom of the screen
+        St::Bottom => "0",
+        // without this command input is not horizontally centered
+        St::Left => "0",
+        St::Width => format!("calc(100% - calc(2 * {}))", OUTER_PADDING),
+        St::Margin => OUTER_PADDING
+    };
     let textarea = textarea![
-        C![
-            C.border_primary,
-            C.border_2,
-            EDGE_STYLE,
-            TEXT_PADDING,
-            C.outline_none,
-            C.overflow_hidden
-        ],
+        &textarea_class,
         attrs! {At::Type => "text", At::Name => "input"},
-        style! {St::Resize => "none", St::Height => height,
-            St::Position => "fixed",
-            // required to fix the commend input to the bottom of the screen
-            St::Bottom => "0",
-            // without this command input is not horizontally centered
-            St::Left => "0",
-            St::Width => format!("calc(100% - calc(2 * {}))", OUTER_PADDING),
-            St::Margin => OUTER_PADDING
-        },
+        &textarea_style,
+        style! {St::WebkitTextFillColor => "rgba(0,0,0,0)", St::BackgroundColor => "rgba(0,0,0,0)"},
         el_ref(&model.command_input),
         input_ev(Ev::Input, |s| GlobalMsg::Home(HomeMsg::Input(s))),
         keyboard_ev("keydown", |ev| (ev.key_code() == 13).then(|| {
@@ -41,11 +45,43 @@ pub fn view(model: &HomeModel) -> impl IntoNodes<GlobalMsg> {
             ev.stop_propagation();
             GlobalMsg::Home(HomeMsg::Submit)
         })),
-        model
-            .context
-            .lex(&model.input)
-            .into_iter()
-            .map(|lexeme| div![lexeme.text])
     ];
-    vec![div, textarea]
+    let spans: Vec<_> = model
+        .context
+        .lex(&model.input)
+        .into_iter()
+        .map(|lexeme| {
+            let colour = match lexeme.category {
+                LexemeCategory::Concept(ConceptKind::New) => {
+                    Some(C.text_new_concept)
+                },
+                LexemeCategory::Concept(ConceptKind::Variable) => {
+                    Some(C.text_variable_concept)
+                },
+                LexemeCategory::Concept(ConceptKind::Concrete) => {
+                    Some(C.text_concrete_concept)
+                },
+                LexemeCategory::Concept(ConceptKind::Abstract) => {
+                    Some(C.text_abstract_concept)
+                },
+                LexemeCategory::OpeningParenthesis {
+                    closing_position: None,
+                }
+                | LexemeCategory::ClosingParenthesis {
+                    opening_position: None,
+                } => Some(C.text_unmatched_parenthesis),
+                _ => None,
+            };
+            span![C![colour], lexeme.text]
+        })
+        .collect();
+    // TODO check whether word wrapping properties are consistent with textarea
+    let syntax_colouring = div![
+        &textarea_style,
+        &textarea_class,
+        // prevents history from being visible inside command input when scrolling
+        C![C.bg_secondary],
+        spans
+    ];
+    vec![bottom_buffer, syntax_colouring, textarea]
 }
