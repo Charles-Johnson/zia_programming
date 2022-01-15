ad1acf8723a56379260c2a700b6f87881900a7ab
Charles Johnson <charlesthomasjohnson0@gmail.com>
2022-01-15 13:14:44 +0900

    Write test for factorial tutorial

--- zia-lang.org/crate/src/page/home/tutorials.rs
+++ zia-lang.org/crate/src/page/home/tutorials.rs
@@ -4,23 +4,33 @@ pub const TUTORIALS: (Tutorial<5>, Tutorial<0>) = (
         steps: [
             TutorialStep {
                 command: "let 1 ! -> 1",
-                explanation: "You can first define the factorial operator, !, for one"
+                explanation: "You can first define the factorial operator, !, for one",
+                #[cfg(test)]
+                expected_evaluation: ""
             },
             TutorialStep {
                 command: "let (_x_ +1) ! -> (_x_ +1) * _x_ !",
-                explanation: "Then define the factorial of the next integer after _x_, _x_ +1, for any _x_ in terms of _x_ !"
+                explanation: "Then define the factorial of the next integer after _x_, _x_ +1, for any _x_ in terms of _x_ !",
+                #[cfg(test)]
+                expected_evaluation: ""
             },
             TutorialStep {
                 command: "let 1 * _x_ -> _x_",
-                explanation: "Need to define the multiplication operator, *. You can start by defining that anything multiplied by one is itself"
+                explanation: "Need to define the multiplication operator, *. You can start by defining that anything multiplied by one is itself",
+                #[cfg(test)]
+                expected_evaluation: ""
             },
             TutorialStep {
                 command: "let 2 := 1 +1",
-                explanation: "Define what two is in terms of the increment operator, +1"
+                explanation: "Define what two is in terms of the increment operator, +1",
+                #[cfg(test)]
+                expected_evaluation: ""
             },
             TutorialStep {
                 command: "2 !",
-                explanation: "Finally you can evaluate two factorial!"
+                explanation: "Finally you can evaluate two factorial!",
+                #[cfg(test)]
+                expected_evaluation: "2"
             }
         ]
     },
@@ -44,4 +54,21 @@ pub struct Tutorial<const N: usize> {
 pub struct TutorialStep {
     pub command: &'static str,
     pub explanation: &'static str,
+    #[cfg(test)]
+    expected_evaluation: &'static str
+}
+
+#[cfg(test)]
+mod test {
+    use zia::multi_threaded::NEW_CONTEXT;
+
+    use super::TUTORIALS;
+
+    #[test]
+    fn factorial_tutorial() {
+        let mut context = NEW_CONTEXT.clone();
+        for step in TUTORIALS.0.steps {
+            assert_eq!(context.execute(step.command), step.expected_evaluation);
+        }
+    }
 }
