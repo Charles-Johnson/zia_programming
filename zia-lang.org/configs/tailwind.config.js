fe956b824121e36658e004b31e084b5b12884811
Charles Johnson <charlesthomasjohnson0@gmail.com>
2022-09-19 00:57:48 +0900

    Override almost all tailwind default config

--- zia-lang.org/configs/tailwind.config.js
+++ zia-lang.org/configs/tailwind.config.js
@@ -13,43 +13,150 @@ Object.fromEntries = Object.fromEntries || ((iterable) => {
   }, {})
 });
 
-module.exports = {
-  theme: {
-    spacing: {
-      2: "2px",
-      5: "5px",
-      6: "6px",
-      8: "8px",
-      10: "10px",
-      16: "16px",
-      20: "20px",
-      24: "24px"
-    },
-    screens: {
-      'sm': '569px',
-      // => @media (min-width: 569px) { ... }
+const colors = {
+  primary: "#0B723B",
+  secondary: "#FFFFFF",
+  variable_concept: "#FFBF00",
+  abstract_concept: "#BF00FF",
+  concrete_concept: "#4000FF",
+  new_concept: "#80FF00",
+  unmatched_parenthesis: "#FF0000"
+};
 
-      'lg': '1025px',
-      // => @media (min-width: 1025px) { ... }
-    },
-    fontFamily: {
-      display: ['Metropolis', 'sans-serif'],
-      monospace: ['Courier New', 'monospace'],
-    },
-    fontSize: { 29: "29px", 45: "45px", 55: "55px" },
-    colors: {
-      primary: "#0B723B",
-      secondary: "#FFFFFF",
-      variable_concept: "#FFBF00",
-      abstract_concept: "#BF00FF",
-      concrete_concept: "#4000FF",
-      new_concept: "#80FF00",
-      unmatched_parenthesis: "#FF0000"
-    },
-    borderRadius: {
-      '28px': '28px'
-    }
+const spacing = {
+  2: '0.5rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  8: '2rem',
+  10: '2.5rem',
+  16: '4rem',
+  20: '5rem',
+  24: '6rem',
+};
+
+const theme = {
+  spacing,
+  screens: {
+    'sm': '569px',
+    // => @media (min-width: 569px) { ... }
+
+    'lg': '1025px',
+    // => @media (min-width: 1025px) { ... }
   },
+  fontFamily: {
+    display: ['Metropolis', 'sans-serif'],
+    monospace: ['Courier New', 'monospace'],
+  },
+  fontSize: { 29: "29px", 45: "45px", 55: "55px" },
+  colors,
+  borderRadius: {
+    '28px': '28px'
+  },
+  columns: {},
+  animation: {},
+  aspectRatio: {},
+  backgroundImage: {},
+  backgroundPsotion: {},
+  backgroundSize: {},
+  blur: {},
+  brightness: {},
+  borderColor: {
+    primary: colors.primary,
+  },
+  borderOpacity: {},
+  borderSpacing: {},
+  borderWidth: {
+    2: spacing[2],
+  },
+  boxShadow: {},
+  boxShadowColor: {},
+  caretColor: {},
+  accentColor: {},
+  contrast: {},
+  content: {},
+  cursor: {},
+  divideColor: {},
+  divideWidth: {},
+  dropShadow: {},
+  fill: {},
+  grayscale: {},
+  hueRotate: {},
+  invert: {},
+  flex: {
+    1: '1 1 0%',
+  },
+  flexBasis: {},
+  flexGrow: {
+    DEFAULT: '1',
+  },
+  flexShrink: {},
+  fontWeight: {
+    thin: '100',
+  },
+  gap: {},
+  gradientColorStops: {},
+  gridAutoColumns: {},
+  gridAutoRows: {},
+  gridColumn: {},
+  gridColumnEnd: {},
+  gridColumnStart: {},
+  gridRow: {},
+  gridRowStart: {},
+  gridRowEnd: {},
+  gridTemplateColumns: {},
+  gridTemplateRows: {},
+  height: { 10: spacing[10], screen: "100vh" },
+  inset: {},
+  keyframes: {},
+  letterSpacing: {},
+  lineHeight: {},
+  listStyleType: {},
+  maxHeight: {},
+  maxWidth: {},
+  minHeight: {},
+  minWidth: {},
+  objectPosition: {},
+  opacity: {},
+  order: {},
+  padding: { 2: spacing[2] },
+  placeholderColor: {},
+  placeholderOpacity: {},
+  outlineColor: {},
+  outlineOffset: {},
+  outlineWidth: {},
+  ringColor: {},
+  ringOffsetColor: {},
+  ringOffsetWidth: {},
+  ringOpacity: {},
+  ringWidth: {},
+  rotate: {},
+  saturate: {},
+  scale: {},
+  scrollMargin: {},
+  scrollPadding: {},
+  sepia: {},
+  skew: {},
+  space: {},
+  stroke: {},
+  strokeWidth: {},
+  textDecorationColor: {},
+  textDecorationThickness: {},
+  textUnderlineOffset: {},
+  textIndent: {},
+  textOpacity: {},
+  transformOrigin: {},
+  transitionDelay: {},
+  transitionDuration: {},
+  transitionProperty: {},
+  transitionTimingFunction: {},
+  translate: {},
+  width: { ...spacing },
+  willChange: {},
+  zIndex: {}
+};
+
+module.exports = {
+  theme,
   variants: {},
   plugins: []
 }
